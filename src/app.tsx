721ef8b77f6e40f125b60a482671ae901189c164
dev <pydev21@gmail.com>
2021-01-14 13:20:45 +0530

    the wheels are in motion!

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import { hot } from 'react-hot-loader';
 import * as React from 'react';
 
-const App = () => <div>The wheels are in motion</div>;
+const App = () => <h2>The wheels are in motion</h2>;
 
 export default hot(module)(App);
