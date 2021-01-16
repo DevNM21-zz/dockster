39d48b2a35078df325d1e85efa8631442928b2a5
dev <pydev21@gmail.com>
2021-01-16 23:07:01 +0530

    add react-router to app and set default view to home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { hot } from 'react-hot-loader';
 import * as React from 'react';
+import {
+  HashRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
 import AppLayout from './AppLayout';
+import DefaultView from './Views/Default';
 
 const App = () => {
   return (
     <>
+    <HashRouter>
     <AppLayout>
+        <Switch>
+          <Route path={'/'} exact>
+            <DefaultView/>
+          </Route>
+        </Switch>
     </AppLayout>
-      </>
+    </HashRouter>
+    </>
   );
 };
 
