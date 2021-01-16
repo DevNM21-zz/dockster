c233e63dafe3bd430f3d0fc0ed1761333fac9ed6
dev <pydev21@gmail.com>
2021-01-16 23:05:52 +0530

    create a default page

---
+++ src/Views/Default/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+
+const Index = () => {
+  return (
+    <>
+      <Container style={{ paddingTop: '40%' }} className={'justify-content-center text-center'}>
+        <h4>It looks so empty!</h4>
+        <p>Let&apos;s start by adding a project.</p>
+        <Button variant={'dark'}>
+          Add Project +
+        </Button>
+      </Container>
+    </>
+  );
+};
+
+export default Index;
