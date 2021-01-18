154d98ad444dc820a252053255c0b1ba8cb5e1a5
dev <pydev21@gmail.com>
2021-01-18 23:27:10 +0530

    create a modal component, an abstract component over bootstrap modal with title, body and close button

---
+++ src/Components/Modal/index.tsx
@@ -0,0 +1,31 @@
+import React, { ReactChild, ReactFragment, ReactPortal } from 'react';
+import ModalHeader from 'react-bootstrap/ModalHeader';
+import ModalTitle from 'react-bootstrap/ModalTitle';
+import ModalBody from 'react-bootstrap/ModalBody';
+import Modal from 'react-bootstrap/Modal';
+import CloseButton from 'react-bootstrap/CloseButton';
+
+interface props {
+  title: string;
+  children: ReactChild | ReactFragment | ReactPortal | null;
+  toggleModal: () => void;
+  show: false;
+  onClose?: () => void;
+  onOpen?: () => void;
+}
+
+const CustomModal = ({ title, show, children, toggleModal, onOpen, onClose }: props): JSX.Element => {
+  return (
+    <div>
+      <Modal show={show} onOpen={onOpen} onExited={onClose}>
+        <ModalHeader>
+          <ModalTitle>{title}</ModalTitle>
+          <CloseButton onClick={toggleModal} />
+        </ModalHeader>
+        <ModalBody>{children}</ModalBody>
+      </Modal>
+    </div>
+  );
+};
+
+export default CustomModal;
