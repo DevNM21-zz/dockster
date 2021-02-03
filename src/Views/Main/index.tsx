23d296c99779febb7201ee0b720d3ab5f16ad588
dev <pydev21@gmail.com>
2021-02-03 22:24:22 +0530

    setup simple project view

--- src/Views/Main/index.tsx
+++ src/Views/Main/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import TabContainer from 'react-bootstrap/TabContainer';
 
 interface Props {
-	projectId: string;
+  projectId: string;
 }
 const Index = ({ projectId }: Props) => {
-	return (
-		<>
-			<h1>Hi {projectId}</h1>
-		</>
-	);
+  return (
+    <>
+      <Tabs defaultActiveKey="2">
+        <Tab eventKey={'Images'} title="Images" key="1">
+          <TabContainer>Hi</TabContainer>
+        </Tab>
+        <Tab eventKey="Containers"  title="Containers" key="2">
+          --- Containers ---
+        </Tab>
+        <Tab eventKey="Networks" title="Networks" key="3">
+          --- Networks ---
+        </Tab>
+      </Tabs>
+    </>
+  );
 };
 
 export default Index;
