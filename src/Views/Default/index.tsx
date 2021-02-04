01fb832bdcb52509aea2328bf05ae12a8e84bce6
dev <pydev21@gmail.com>
2021-02-04 19:07:39 +0530

    Main component: redirect to first project on first fload

--- src/Views/Default/index.tsx
+++ src/Views/Default/index.tsx
@@ -4,6 +4,7 @@ import { connect, ConnectedProps } from 'react-redux';
 import AddProjectInfo from './AddProjectInfo';
 import { useParams } from 'react-router-dom';
 import Main from '../Main';
+import { Redirect } from 'react-router-dom';
 
 const mapStateToProps = ({ projects }: IRootState) => ({ projects });
 const connector = connect(mapStateToProps);
@@ -11,8 +12,9 @@ type PropsFromRedux = ConnectedProps<typeof connector>;
 
 const Index: React.FC<PropsFromRedux> = (props: PropsFromRedux) => {
   const { projectId }: { projectId: string } = useParams();
-  if (projectId) return <Main projectId={projectId} />
-  return <>{props.projects.projects.length > 0 ? <div>Projects detected</div> : <AddProjectInfo />}</>;
+  if (projectId) return <Main projectId={projectId} />;
+  if (props.projects.projects.length > 0) return <Redirect to={`/${props.projects.projects[0]._id}`} />;
+  else return <AddProjectInfo />;
 };
 
 export default connector(Index);
