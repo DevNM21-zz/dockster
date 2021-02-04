8da9044176e06d8215a14711ad40f7e048b6fb1c
dev <pydev21@gmail.com>
2021-02-04 21:56:37 +0530

    create container Card

---
+++ src/Views/Main/Containers/ContainerCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+interface Ports {
+  privatePort: number;
+  publicPort: number;
+  type: string;
+}
+
+interface Props {
+	Id: string;
+  name: string;
+  image: string;
+  created: string;
+  state: string;
+  status: string;
+  ports?: Ports[];
+}
+
+const ContainerCard: React.FC<Props> = ({ name, Id, image, created }: Props) => {
+  return (
+    <Card style={{ marginLeft: '5px' }}>
+      <Card.Body>
+        <p>
+          {name} ({Id}) from {image}
+        </p>
+        <Card.Footer>
+          <p>Created at {created}</p>
+        </Card.Footer>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ContainerCard;
